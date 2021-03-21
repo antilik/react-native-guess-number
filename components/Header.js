96346c2d6a2c78ff5f40ff6b5e7430c02f24ba3e
Anton Degnera <antilik@gmail.com>
2021-03-22 01:31:10 +0200

    styling the main container, added asset.

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = ({ title }) => {
+  // console.log('title', title);
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 90,
+    paddingTop: 26,
+    backgroundColor: '#f7287b',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    color: 'black',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default Header;
