1e92405740c2e239d615ac0739046bcf7b347faa
Anton Degnera <anton.degnera@globallogic.com>
2021-03-25 00:24:35 +0200

    logic for Input. UI style for Input. Some other functional.

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+
+import Card from '../components/Card';
+import Input from '../components/Input';
+import Colors from '../constants/colors';
+
+const StartGameScreen = () => {
+  const [value, setValue] = useState('');
+  const [isConfirmed, setIsConfirmed] = useState(false);
+  const [selectedUserNumber, setSelectedUserNumber] = useState();
+
+  const hideKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
+  const inputHandler = (inputText) => {
+    setValue(inputText.replace(/[^0-9]/g, ''));
+  };
+
+  const resetBtnHandler = () => {
+    setValue('');
+    setIsConfirmed(false);
+  };
+
+  const confirmBtnHandler = () => {
+    const chosenNumber = parseInt(value);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      return null;
+    }
+
+    setSelectedUserNumber(chosenNumber);
+    setIsConfirmed(true);
+    setValue('');
+  };
+
+  const showChosenNumber = () => {
+    return (
+      <Text
+        style={styles.chosenNumberText}
+      >Chosen Number: {selectedUserNumber}</Text>
+    );
+  };
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={hideKeyboard}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game!</Text>
+        <Card
+          style={{
+            width: 300,
+            maxWidth: '90%',
+            alignItems: 'center',
+          }}
+        >
+          <Text>Select a Number</Text>
+          <Input
+            style={styles.input}
+            inputProps={{
+              keyboardType: 'phone-pad',
+              autoFocus: true,
+              blurOnSubmit: true,
+              autoCapitalize: 'none',
+              autoCorrect: false,
+              maxLength: 2,
+              value: value,
+              onChangeText: inputHandler,
+            }}
+          />
+          <View style={styles.buttonsContainer}>
+            <View style={styles.btnWrapper}>
+              <Button
+                title='Reset'
+                onPress={resetBtnHandler}
+                color={Colors.btnDisagree}
+              />
+            </View>
+            <View style={styles.btnWrapper}>
+              <Button
+                title='Confirm'
+                onPress={confirmBtnHandler}
+                color={Colors.btnAgree}
+              />
+            </View>
+          </View>
+        </Card>
+        {isConfirmed ? showChosenNumber() : null}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 20,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+  },
+  btnWrapper: {
+    width: 100,
+  },
+  input: {
+    width: 80,
+    textAlign: 'center',
+  },
+  chosenNumberText: {
+    marginTop: 80,
+  },
+});
+
+export default StartGameScreen;
